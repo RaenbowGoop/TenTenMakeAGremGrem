0065467246a4f2a09823d5ed2fa3a58b8ce843cb
Amanda Le <leamanda35@gmail.com>
2024-08-18 18:06:35 -0700

    Implemented Main Menu Buttons and Backgrounds + button SFX

---
+++ Assets/Scripts/GeneralAssetScripts/ButtonFunctions.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public AudioSource mySounds;
+    public AudioClip hoverSound;
+    public AudioClip clickSound;
+
+    // Play hover SFX once on hover
+    public void HoverSound()
+    {
+        mySounds.volume = 0.5f;
+        mySounds.PlayOneShot(hoverSound);
+    }
+
+    // Play Click SFX once on Click
+    public void ClickSound()
+    {
+        mySounds.volume = 0.5f;
+        mySounds.PlayOneShot(clickSound);
+    }
+}
