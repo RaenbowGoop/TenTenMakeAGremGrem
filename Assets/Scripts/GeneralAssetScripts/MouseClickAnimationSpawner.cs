024e9ebd27b8d96801e603bd4a5bd2c4f6f3beae
Amanda Le <leamanda35@gmail.com>
2024-08-30 20:46:16 -0700

    Made mouse click animation change to random colors (with color channel bounds)

--- Assets/Scripts/GeneralAssetScripts/MouseClickAnimationSpawner.cs
+++ Assets/Scripts/GeneralAssetScripts/MouseClickAnimationSpawner.cs
@@ -1,22 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MouseClickAnimationSpawner : MonoBehaviour
 {
     
     [SerializeField] Animator mouseAnim;
+    [SerializeField] TextMeshProUGUI animationText;
     [SerializeField] GameObject mouseAnimObj;
+    System.Random rng = new System.Random();
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            // Generate Random Color (lowerbound: 0.3f)
+            float red = (float)rng.NextDouble()*0.7f;
+            float green = (float)rng.NextDouble()* 0.7f;
+            float blue = (float)rng.NextDouble()* 0.7f;
+            animationText.color = new Color(red, green, blue, 1.0f);
+
+            // Set animation object to mouse position
             mouseAnimObj.transform.position = Input.mousePosition;
+
+            // trigger animation condition
             mouseAnim.SetBool("Active", true);
         }
         if (Input.GetMouseButtonUp(0))
         {
+            // turn off animation trigger
             mouseAnim.SetBool("Active", false);
         }
     }
