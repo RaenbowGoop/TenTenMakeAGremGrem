da4d269d5b811afa64d76d15ee6e7551f6dae0a3
Amanda Le <leamanda35@gmail.com>
2024-09-02 18:36:33 -0700

    Update GremGachaDisplay and GremGachaDisplay
    Co-Authored-By: LeSakiSaru <67242282+LeSakiSaru@users.noreply.github.com>

--- Assets/Scripts/MakeAGrem/GremGachaDisplay.cs
+++ Assets/Scripts/MakeAGrem/GremGachaDisplay.cs
@@ -6,31 +6,40 @@ using UnityEngine.UI;
 public class GremGachaDisplay : MonoBehaviour
 {
     [SerializeField] GremGachaManager gremGachaManager;
+    bool isDisplayed;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Display Assets in Model
-        // Head
-        this.transform.GetChild(10).GetComponent<Image>().sprite = gremGachaManager.head.setHeadFront;
-        this.transform.GetChild(1).GetComponent<Image>().sprite = gremGachaManager.head.setHeadBack;
+        isDisplayed = false;
+    }
 
-        // Torso
-        this.transform.GetChild(9).GetComponent<Image>().sprite = gremGachaManager.torso.setTorsoFront;
-        this.transform.GetChild(7).GetComponent<Image>().sprite = gremGachaManager.torso.setTorsoMiddle;
-        this.transform.GetChild(4).GetComponent<Image>().sprite = gremGachaManager.torso.setTorsoBack;
+    void Update()
+    {
+        if (!isDisplayed)
+        {
+            // Display Assets in Model
+            // Head
+            this.transform.GetChild(10).GetComponent<Image>().sprite = gremGachaManager.head.setHeadFront;
+            this.transform.GetChild(1).GetComponent<Image>().sprite = gremGachaManager.head.setHeadBack;
 
-        // Legs
-        this.transform.GetChild(8).GetComponent<Image>().sprite = gremGachaManager.legs.setLegsFront;
-        this.transform.GetChild(6).GetComponent<Image>().sprite = gremGachaManager.legs.setLegsMiddle;
-        this.transform.GetChild(2).GetComponent<Image>().sprite = gremGachaManager.legs.setLegsBack;
+            // Torso
+            this.transform.GetChild(9).GetComponent<Image>().sprite = gremGachaManager.torso.setTorsoFront;
+            this.transform.GetChild(7).GetComponent<Image>().sprite = gremGachaManager.torso.setTorsoMiddle;
+            this.transform.GetChild(4).GetComponent<Image>().sprite = gremGachaManager.torso.setTorsoBack;
 
-        // Shoes
-        this.transform.GetChild(5).GetComponent<Image>().sprite = gremGachaManager.shoes.setShoesFront;
-        this.transform.GetChild(3).GetComponent<Image>().sprite = gremGachaManager.shoes.setShoesBack;
+            // Legs
+            this.transform.GetChild(8).GetComponent<Image>().sprite = gremGachaManager.legs.setLegsFront;
+            this.transform.GetChild(6).GetComponent<Image>().sprite = gremGachaManager.legs.setLegsMiddle;
+            this.transform.GetChild(2).GetComponent<Image>().sprite = gremGachaManager.legs.setLegsBack;
 
-        // Back Piece
-        this.transform.GetChild(0).GetComponent<Image>().sprite = gremGachaManager.backPiece.setBackPiece;
+            // Shoes
+            this.transform.GetChild(5).GetComponent<Image>().sprite = gremGachaManager.shoes.setShoesFront;
+            this.transform.GetChild(3).GetComponent<Image>().sprite = gremGachaManager.shoes.setShoesBack;
 
+            // Back Piece
+            this.transform.GetChild(0).GetComponent<Image>().sprite = gremGachaManager.backPiece.setBackPiece;
+            isDisplayed = true;
+        }
     }
 }
