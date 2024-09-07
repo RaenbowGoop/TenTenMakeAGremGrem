21cf3b1f63fcbfa298318ac0cd28eba26bfcf1d2
Amanda Le <leamanda35@gmail.com>
2024-09-06 22:16:09 -0700

    Made Cafe Saki cycle lines if target time is entered or exited in make a grem scene

--- Assets/Scripts/MakeAGrem/CafeSakiMessageManager.cs
+++ Assets/Scripts/MakeAGrem/CafeSakiMessageManager.cs
@@ -13,6 +13,7 @@ public class CafeSakiMessageManager : MonoBehaviour
     [SerializeField] ColorTimeManager colorTimeManager;
     System.DateTime localDate;
     System.Random randNumGen;
+    bool currentState;
 
 
     // Start is called before the first frame update
@@ -24,29 +25,24 @@ public class CafeSakiMessageManager : MonoBehaviour
         // Set up Random Number Generator
         randNumGen = new System.Random();
 
-        // determine which list of messages to choose from
-        if (checkForRareLine())
+        // Set saki message
+        currentState = checkIfProperTime();
+        setSakiLine();
+    }
+    void Update()
+    {
+        // check if exiting or entering target time (if so, change message accordingly)
+        bool isProperTime = checkIfProperTime();
+        if (currentState != isProperTime)
         {
-            int lineIndex = randNumGen.Next(0, cafeSakiLinesRareLines.Count);
-            messageText.text = cafeSakiLinesRareLines[lineIndex];
-        } else {
-            if (checkIfProperTime())
-            {
-                // Set random line to message box
-                int lineIndex = randNumGen.Next(0, cafeSakiLinesDuringTime.Count);
-                messageText.text = cafeSakiLinesDuringTime[lineIndex];
-            }
-            else
-            {
-                // Set random line to message box
-                int lineIndex = randNumGen.Next(0, cafeSakiLinesNotDuringTime.Count);
-                messageText.text = cafeSakiLinesNotDuringTime[lineIndex];
-            }
+            currentState = isProperTime;
+            setSakiLine();
         }
     }
-
     bool checkIfProperTime()
     {
+        localDate = System.DateTime.Now;
+
         // Return true if time is target time or within grace period after target time
         if (localDate.Hour % 12 == colorTimeManager.targetHour && localDate.Minute >= colorTimeManager.targetMinute && localDate.Minute <= colorTimeManager.targetMinute + colorTimeManager.gracePeriod)
         {
@@ -68,4 +64,30 @@ public class CafeSakiMessageManager : MonoBehaviour
         }
         return false;
     }
+
+    void setSakiLine()
+    {
+        // determine which list of messages to choose from
+        if (checkForRareLine())
+        {
+            int lineIndex = randNumGen.Next(0, cafeSakiLinesRareLines.Count);
+            messageText.text = cafeSakiLinesRareLines[lineIndex];
+        }
+        else
+        {
+            if (checkIfProperTime())
+            {
+                // Set random line to message box
+                int lineIndex = randNumGen.Next(0, cafeSakiLinesDuringTime.Count);
+                messageText.text = cafeSakiLinesDuringTime[lineIndex];
+            }
+            else
+            {
+                // Set random line to message box
+                int lineIndex = randNumGen.Next(0, cafeSakiLinesNotDuringTime.Count);
+                messageText.text = cafeSakiLinesNotDuringTime[lineIndex];
+            }
+        }
+    }
 }
+
