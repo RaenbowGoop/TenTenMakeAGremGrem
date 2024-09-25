2e6594fe15ed2623e043180a980dcf261825ac1c
Amanda Le <leamanda35@gmail.com>
2024-09-25 16:47:03 -0700

    Implemented Edge Case where no grems have been pulled yet (for highest and lowest grems)

--- Assets/Scripts/PlayerSetStatistics/ExtremeSetDisplay.cs
+++ Assets/Scripts/PlayerSetStatistics/ExtremeSetDisplay.cs
@@ -9,14 +9,18 @@ public class ExtremeSetDisplay : MonoBehaviour
     // Cosmetic Set Database
     [SerializeField] CosmeticSetDatabase csdb;
 
+    // Dummy Gooper for placeholding
+    [SerializeField] CosmeticSet dummyGooper;
+
     // Set Statistics
     SetStatisticManager setStatisticManager;
 
     // Grem Display Game Objects
     [SerializeField] GameObject gremModel;
     [SerializeField] GameObject gremDetails;
-    
+
     // Cosmetic Sets for Highest Score
+    GremCapsule highestGrem;
     CosmeticSet headHigh;
     CosmeticSet torsoHigh;
     CosmeticSet legsHigh;
@@ -24,6 +28,7 @@ public class ExtremeSetDisplay : MonoBehaviour
     CosmeticSet backPieceHigh;
 
     // Cosmetic Sets for Lowest Score
+    GremCapsule lowestGrem;
     CosmeticSet headLow;
     CosmeticSet torsoLow;
     CosmeticSet legsLow;
@@ -36,32 +41,59 @@ public class ExtremeSetDisplay : MonoBehaviour
         // Find Set StatisticManager
         setStatisticManager = GameObject.FindWithTag("Player").GetComponent<SetStatisticManager>();
 
-        // Create dictionary for cosmetic set for searching
-        Dictionary<string, CosmeticSet> cosmeticSetDictionary = new Dictionary<string, CosmeticSet>();
-        foreach (CosmeticSet set in csdb.cosmeticSets)
+        // Substitute dummy grem grem if not grems have been rolled before
+        if (!setStatisticManager.highestScoreGrem.HasGrem || !setStatisticManager.lowestScoreGrem.HasGrem)
         {
-            cosmeticSetDictionary[set.setName] = set;
+            // Set Grem Capsules for Scores + multiplier
+            highestGrem = new GremCapsule(0, 0, 0.0f, "DUMMY GOOPER", "DUMMY GOOPER", "DUMMY GOOPER", "DUMMY GOOPER", "DUMMY GOOPER", true);
+            lowestGrem = highestGrem;
+
+            // Set Cosmetic Sets for highest grem
+            headHigh = dummyGooper;
+            torsoHigh = dummyGooper;
+            legsHigh = dummyGooper;
+            shoesHigh = dummyGooper;
+            backPieceHigh = dummyGooper;
+
+            // Set Cosmetic Sets for lowest grem
+            headLow = dummyGooper;
+            torsoLow = dummyGooper;
+            legsLow = dummyGooper;
+            shoesLow = dummyGooper;
+            backPieceLow = dummyGooper;
+
+        } else {
+            // Create dictionary for cosmetic set for searching
+            Dictionary<string, CosmeticSet> cosmeticSetDictionary = new Dictionary<string, CosmeticSet>();
+            foreach (CosmeticSet set in csdb.cosmeticSets)
+            {
+                cosmeticSetDictionary[set.setName] = set;
+            }
+
+            // Set Cosmetic Sets for highest grem
+            highestGrem = setStatisticManager.highestScoreGrem;
+            headHigh = cosmeticSetDictionary[highestGrem.Head];
+            torsoHigh = cosmeticSetDictionary[highestGrem.Torso];
+            legsHigh = cosmeticSetDictionary[highestGrem.Legs];
+            shoesHigh = cosmeticSetDictionary[highestGrem.Shoes];
+            backPieceHigh = cosmeticSetDictionary[highestGrem.BackPiece];
+
+            // Set Cosmetic Sets for lowest grem
+            lowestGrem = setStatisticManager.lowestScoreGrem;
+            headLow = cosmeticSetDictionary[lowestGrem.Head];
+            torsoLow = cosmeticSetDictionary[lowestGrem.Torso];
+            legsLow = cosmeticSetDictionary[lowestGrem.Legs];
+            shoesLow = cosmeticSetDictionary[lowestGrem.Shoes];
+            backPieceLow = cosmeticSetDictionary[lowestGrem.BackPiece];
+
         }
 
-        // Set Cosmetic Sets for highest grem
-        GremCapsule highestGrem = setStatisticManager.highestScoreGrem;
-        headHigh = cosmeticSetDictionary[highestGrem.Head];
-        torsoHigh = cosmeticSetDictionary[highestGrem.Torso];
-        legsHigh = cosmeticSetDictionary[highestGrem.Legs];
-        shoesHigh = cosmeticSetDictionary[highestGrem.Shoes];
-        backPieceHigh = cosmeticSetDictionary[highestGrem.BackPiece];
-
-        // Set Cosmetic Sets for lowest grem
-        GremCapsule lowestGrem = setStatisticManager.lowestScoreGrem;
-        headLow = cosmeticSetDictionary[lowestGrem.Head];
-        torsoLow = cosmeticSetDictionary[lowestGrem.Torso];
-        legsLow = cosmeticSetDictionary[lowestGrem.Legs];
-        shoesLow = cosmeticSetDictionary[lowestGrem.Shoes];
-        backPieceLow = cosmeticSetDictionary[lowestGrem.BackPiece];
+        // display Highest grem initially
+        displayHighestScoreGrem();
     }
 
 
-    void displayGrem(CosmeticSet head, CosmeticSet torso, CosmeticSet legs, CosmeticSet shoes, CosmeticSet backPiece)
+    void displayGrem(CosmeticSet head, CosmeticSet torso, CosmeticSet legs, CosmeticSet shoes, CosmeticSet backPiece, GremCapsule gremCapsule)
     {
         // Display Assets in Model
         // Head
@@ -93,16 +125,18 @@ public class ExtremeSetDisplay : MonoBehaviour
         gremModel.transform.GetChild(0).GetComponentInChildren<Image>().sprite = backPiece.setBackPieceBack;
 
         // DISPLAY SET POINTS AND DETAILS
-
+        gremDetails.transform.GetChild(0).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = gremCapsule.Score.ToString("N0") + " pts";
+        gremDetails.transform.GetChild(1).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = gremCapsule.BasePoints.ToString("N0") + " pts";
+        gremDetails.transform.GetChild(2).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "x" + gremCapsule.Multiplier.ToString();
     }
 
     public void displayHighestScoreGrem()
     {
-        displayGrem(headHigh, torsoHigh, legsHigh, shoesHigh, backPieceHigh);
+        displayGrem(headHigh, torsoHigh, legsHigh, shoesHigh, backPieceHigh, highestGrem);
     }
 
     public void displayLowestScoreGrem()
     {
-        displayGrem(headLow, torsoLow, legsLow, shoesLow, backPieceLow);
+        displayGrem(headLow, torsoLow, legsLow, shoesLow, backPieceLow, lowestGrem);
     }
 }
