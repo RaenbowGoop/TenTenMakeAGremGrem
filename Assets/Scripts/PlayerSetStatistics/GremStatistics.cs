47da39b4c57b0e6cb8469bba7fa8d3f55ca9e3ea
Amanda Le <leamanda35@gmail.com>
2024-09-25 19:44:50 -0700

    Added background for extreme grem stats
    Co-Authored-By: LeSakiSaru <67242282+LeSakiSaru@users.noreply.github.com>

--- Assets/Scripts/PlayerSetStatistics/GremStatistics.cs
+++ Assets/Scripts/PlayerSetStatistics/GremStatistics.cs
@@ -14,7 +14,7 @@ public class GremStatistics : MonoBehaviour
         SetStatisticManager setStatisticManager = GameObject.FindWithTag("Player").GetComponent<SetStatisticManager>();
 
         // Display number of legal and illegal grems made
-        gremStatisticsDisplay.transform.GetChild(1).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = setStatisticManager.legalGremsMade.ToString("N0");
-        gremStatisticsDisplay.transform.GetChild(2).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = setStatisticManager.illegalGremsMade.ToString("N0");
+        gremStatisticsDisplay.transform.GetChild(1).transform.GetComponent<TextMeshProUGUI>().text = "Legal Grems Made: " + setStatisticManager.legalGremsMade.ToString("N0");
+        gremStatisticsDisplay.transform.GetChild(2).transform.GetComponent<TextMeshProUGUI>().text = "Illegal Grems Made: " + setStatisticManager.illegalGremsMade.ToString("N0");
     }
 }
