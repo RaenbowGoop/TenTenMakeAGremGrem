abaad1c14ff6e4b4b9684e4fbff56cfadcdef60f
LeSakiSaru <lesakisaru@gmail.com>
2024-12-09 22:02:34 -0800

    Implemented ScreenCapture Feature to MakeAGrem Page
    - screenshotting for Desktop and Mobile has been implemented
    - webGL doesn't work currently

--- Assets/Scripts/Profile/PlayerStatisticsManager.cs
+++ Assets/Scripts/Profile/PlayerStatisticsManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using Unity.VisualScripting;
-using UnityEngine.WSA;
 
 public class CosmeticSetCountPair : System.IComparable<CosmeticSetCountPair>, System.IEquatable<CosmeticSetCountPair>
 {
