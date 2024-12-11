196893e61064e803d4e5c1fbf86daf1ac4e1d39b
Amanda Le <leamanda35@gmail.com>
2024-12-10 17:21:36 -0800

    Fixed Android Screenshot Feature and Implemented Screenshot Notification Message + Animations

--- Assets/Scripts/MakeAGrem/ColorTimeManager.cs
+++ Assets/Scripts/MakeAGrem/ColorTimeManager.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Globalization;
+﻿using UnityEngine;
 using UnityEngine.Playables;
 using Unity​Engine.Rendering.PostProcessing;
 
--- Assets/Scripts/ScreenCaptureScript.cs
+++ Assets/Scripts/ScreenCaptureScript.cs
@@ -4,9 +4,14 @@ using System;
 using System.IO;
 using System.Linq;
 using SFB;
+using UnityEngine.Playables;
+using TMPro;
 
 public class ScreenCapture : MonoBehaviour
 {
+    [SerializeField] private PlayableDirector screenshotNotificationAnimation;
+    [SerializeField] private GameObject screenshotNotificationObject;
+
     // only allow screenshot button on supported platforms
     RuntimePlatform[] allowedPlatforms =    {   RuntimePlatform.IPhonePlayer,
                                                 RuntimePlatform.Android,
@@ -28,6 +33,9 @@ public class ScreenCapture : MonoBehaviour
         StartCoroutine(captureAndSaveScreenshot());
     }
     private IEnumerator captureAndSaveScreenshot() {
+        // Hide Screenshot Notification
+        screenshotNotificationObject.SetActive(false);
+
         yield return new WaitForEndOfFrame();
 
         int width = Screen.width;
@@ -38,6 +46,7 @@ public class ScreenCapture : MonoBehaviour
         texture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
         texture.Apply();
 
+
         // encode texture into image
         byte[] data = texture.EncodeToPNG();
         string filename = "gooper-" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
@@ -45,16 +54,32 @@ public class ScreenCapture : MonoBehaviour
         // check platform
         RuntimePlatform platform = Application.platform;
         if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer) {
-            saveScreenshotOnMobile(data, filename);
+            saveScreenshotOnMobile(texture, filename);
         } else {
             saveScreenshotOnDesktopAndWebGL(data, filename);
         }
 
         Destroy(texture);
+
+        // Unhidde Screenshot Notification
+        screenshotNotificationObject.SetActive(true);
     }
 
-    void saveScreenshotOnMobile(byte[] data, String filename) {
-        NativeGallery.SaveImageToGallery(data, "My Grems", filename);
+    void saveScreenshotOnMobile(Texture2D data, String filename) {
+        NativeGallery.SaveImageToGallery(data, "My Grems", filename, (callback, path) =>
+        {
+            if (callback == false)
+            {
+                screenshotNotificationObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Failed to Save Screenshot to Camera Roll.";
+                screenshotNotificationAnimation.Stop();
+                screenshotNotificationAnimation.Play();
+            } else {
+                screenshotNotificationObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Screenshot Saved Under \"My Grems\" Album.";
+                screenshotNotificationAnimation.Stop();
+                screenshotNotificationAnimation.Play();
+            }
+        }
+        );
     }
 
     void saveScreenshotOnDesktopAndWebGL(byte[] data, String filename)
@@ -62,7 +87,20 @@ public class ScreenCapture : MonoBehaviour
         string panelTitle = "Select Folder";
         string defaultDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
         string extension = "png";
-        StandaloneFileBrowser.SaveFilePanelAsync(panelTitle, defaultDirectory, filename, extension, (string path) => { File.WriteAllBytes(path, data); });
+        StandaloneFileBrowser.SaveFilePanelAsync(panelTitle, defaultDirectory, filename, extension, (string path) => {
+            try
+            {
+                File.WriteAllBytes(path, data);
+                screenshotNotificationObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Screenshot Saved!";
+                screenshotNotificationAnimation.Stop();
+                screenshotNotificationAnimation.Play();
+            }
+            catch (Exception e) {
+                screenshotNotificationObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Failed to Save Screenshot.";
+                screenshotNotificationAnimation.Stop();
+                screenshotNotificationAnimation.Play();
+            }
+        });
     }
 
     /* void saveScreenshotOnDesktop(byte[] data, String filename) {
